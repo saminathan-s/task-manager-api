3307886f3f933d99fb135734ac62378456a62563
Saminathan S <Saminathan_S@Singaporeair.com.sg>
2020-07-03 20:56:18 +0800

    removed sharp and mongodb dependencies

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const multer= require('multer');
-const sharp = require('sharp');
 const User = require('../models/user');
 const auth = require('../middlewares/auth');
 
@@ -102,8 +101,7 @@ const upload = multer({
 })
 
 router.post('/users/me/avatar',auth,upload.single('avatar'),async(req,res)=>{
-    const buffer = await sharp(req.file.buffer).resize({width:250,height:250}).png().toBuffer();
-    req.user.avatar=buffer;
+    req.user.avatar=req.file.buffer;
     await req.user.save();
     res.send();
 },(error,req,res,next)=>{
